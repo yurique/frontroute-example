665188d4cca7973fc20cf9cb93e2cbc603b2e353
Iurii Malchenko <i@yurique.com>
2021-06-05 14:28:54 +0300

    deps bump

--- project/DependencyVersions.scala
+++ project/DependencyVersions.scala
@@ -1,8 +1,8 @@
 object DependencyVersions {
 
-  val scala = "2.13.5"
+  val scala = "2.13.6"
   val laminar = "0.13.0"
-  val frontroute = "0.13.1"
-  val scribe = "3.5.4"
+  val frontroute = "0.13.2"
+  val scribe = "3.5.5"
 
 }
